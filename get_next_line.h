bb9d49d8ee411aef89bf23d4e8a727549a0fa48d
Sindiso DUBE <sdube@e4r6p3.wtc.co.za>
2017-08-25 11:01:16 +0200

    Get_Next_Line

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdube <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/21 10:25:21 by sdube             #+#    #+#             */
+/*   Updated: 2016/10/24 13:12:00 by sdube            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
